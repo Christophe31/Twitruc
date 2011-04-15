d3ce957626594356eea2b29fea6d1c6b7a83aec8
Christophe Narbonne <christophe31@gmail.com>
2011-04-15 21:54:06 +0200

    Add Twitterizer and account management from last test project.

---
+++ Twitruc/Controllers/UserController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twitterizer;
+using Twitruc.Models;
+using System.Web.Routing;
+using System.Configuration;
+
+namespace Twitruc.Controllers
+{
+    public class UserController : Controller
+    {
+		public UserService userManager { get; set; }
+
+		protected override void Initialize(RequestContext requestContext) {
+			userManager = userManager ?? new UserService();
+			base.Initialize(requestContext);
+		}
+
+		// **************************************
+		// URL: /User/LogOn
+		// **************************************
+
+		public ActionResult LogOn() {
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult LogOn(LogOnForm model, string returnUrl) {
+			if (ModelState.IsValid) {
+				if (userManager.ValidateUser(model.Nickname, model.Password)) {
+					userManager.SignIn(model.Nickname, model.RememberMe);
+					Session["nickname"] = model.Nickname;
+					Session.Timeout = 100000;
+					if (Url.IsLocalUrl(returnUrl))
+						return Redirect(returnUrl);
+					else
+						return RedirectToAction("Index", "Home");
+				} else
+					ModelState.AddModelError("", "The user name or password provided is incorrect.");
+			}
+
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
+
+		// **************************************
+		// URL: /User/LogOff
+		// **************************************
+
+		public ActionResult LogOff() {
+			userManager.SignOut();
+			Session.Clear();
+
+			return RedirectToAction("Index", "Home");
+		}
+
+		// **************************************
+		// URL: /User/Register
+		// **************************************
+
+		public ActionResult Register() {
+			ViewBag.PasswordLength = userManager.MinPasswordLength;
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Register(RegisterForm model) {
+			if (ModelState.IsValid) {
+				if (userManager.CreateUser(model.Name, model.Nickname, model.Password, model.Email)) {
+					userManager.SignIn(model.Nickname, false /* createPersistentCookie */);
+					return RedirectToAction("Index", "Home");
+				}
+			}
+
+			// If we got this far, something failed, redisplay form
+			ViewBag.PasswordLength = userManager.MinPasswordLength;
+			return View(model);
+		}
+
+		// **************************************
+		// URL: /User/ChangePassword
+		// **************************************
+
+		public ActionResult ChangePassword() {
+			EntityLib.User usr;
+			if (String.IsNullOrEmpty(Session["nickname"] as String))
+				return RedirectToAction("LogOn", "Account", new ReturnUrl(this.HttpContext.Request.RawUrl));
+			else
+				usr = userManager.getUser(Session["nickname"] as string);
+
+			ViewBag.PasswordLength = userManager.MinPasswordLength;
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult ChangePassword(ChangePasswordForm model) {
+			EntityLib.User usr;
+			if (String.IsNullOrEmpty(Session["nickname"] as String))
+				return RedirectToAction("LogOn", "Account", new ReturnUrl(this.HttpContext.Request.RawUrl));
+			else
+				usr = userManager.getUser(Session["nickname"] as string);
+
+			if (ModelState.IsValid) {
+				if (userManager.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword)) {
+					return RedirectToAction("ChangePasswordSuccess");
+				} else {
+					ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
+				}
+			}
+
+			// If we got this far, something failed, redisplay form
+			ViewBag.PasswordLength = userManager.MinPasswordLength;
+			return View(model);
+		}
+
+		// **************************************
+		// URL: /User/ChangePasswordSuccess
+		// **************************************
+
+		public ActionResult ChangePasswordSuccess() {
+			return View();
+		}
+
+		// **************************************
+		// URL: /User/Callback
+		// **************************************
+
+		public ActionResult Callback() {
+			EntityLib.User usr;
+			if (String.IsNullOrEmpty(Session["nickname"] as String))
+				return RedirectToAction("LogOn", "Account", new ReturnUrl(this.HttpContext.Request.RawUrl));
+			else
+				usr = userManager.getUser(Session["nickname"] as string);
+
+			var atoken = OAuthUtility.GetAccessTokenDuringCallback(ConfigurationManager.AppSettings["consumerkey"], ConfigurationManager.AppSettings["consumersecret"]);
+			this.userManager.UpdateTwitterAccount(atoken,usr);
+
+			return RedirectToAction("About", "Home");
+		}
+
+		// **************************************
+		// URL: /User/BeginAuth
+		// **************************************
+
+		public ActionResult BeginAuth() {
+			EntityLib.User usr;
+			if (String.IsNullOrEmpty(Session["nickname"] as String))
+				return RedirectToAction("LogOn", "Account", new ReturnUrl(this.HttpContext.Request.RawUrl));
+			else 
+				usr = userManager.getUser(Session["nickname"] as string);
+
+			var tokens = OAuthUtility.GetRequestToken(ConfigurationManager.AppSettings["consumerkey"], ConfigurationManager.AppSettings["consumersecret"], "http://localhost/Account/Callback");
+			var link = OAuthUtility.BuildAuthorizationUri(tokens.Token).AbsoluteUri;
+
+			return this.Redirect(link);
+		}
+
+
+	}
+
+}
---
+++ Twitruc/Models/UserModel.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Twitterizer;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Twitruc.Models {
+		#region Models
+		public class ChangePasswordForm {
+			[Required]
+			[DataType(DataType.Password)]
+			[Display(Name = "Current password")]
+			public string OldPassword { get; set; }
+
+			[Required]
+			[DataType(DataType.Password)]
+			[Display(Name = "New password")]
+			public string NewPassword { get; set; }
+
+			[DataType(DataType.Password)]
+			[Display(Name = "Confirm new password")]
+			[Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+			public string ConfirmPassword { get; set; }
+		}
+
+		public class LogOnForm {
+			[Required]
+			[Display(Name = "User name")]
+			public string Nickname { get; set; }
+
+			[Required]
+			[DataType(DataType.Password)]
+			[Display(Name = "Password")]
+			public string Password { get; set; }
+
+			[Display(Name = "Remember me?")]
+			public bool RememberMe { get; set; }
+		}
+
+		public class RegisterForm {
+			[Required]
+			[Display(Name = "User name")]
+			public string Name { get; set; }
+
+			[Required]
+			[Display(Name = "Nickname")]
+			public string Nickname { get; set; }
+
+			[Required]
+			[DataType(DataType.EmailAddress)]
+			[Display(Name = "Email address")]
+			public string Email { get; set; }
+
+			[Required]
+			[DataType(DataType.Password)]
+			[Display(Name = "Password")]
+			public string Password { get; set; }
+
+			[DataType(DataType.Password)]
+			[Display(Name = "Confirm password")]
+			[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+			public string ConfirmPassword { get; set; }
+		}
+		#endregion
+
+		#region Services
+		public class UserService {
+			private EntityLib.Model1Container db = new EntityLib.Model1Container();
+			public int MinPasswordLength {
+				get {
+					return 4;
+				}
+			}
+
+			public bool ValidateUser(string nickname, string password) {
+				if (String.IsNullOrEmpty(nickname)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+				if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
+				return db.Users.Any(u => u.Nickname == nickname && u.Password == password);
+			}
+
+			public bool CreateUser(string userName, string nickname, string password, string email) {
+				if (String.IsNullOrEmpty(userName))
+					throw new ArgumentException("Value cannot be null or empty.", "userName");
+				if (String.IsNullOrEmpty(nickname))
+					throw new ArgumentException("Value cannot be null or empty.", "nickname");
+				if (String.IsNullOrEmpty(password))
+					throw new ArgumentException("Value cannot be null or empty.", "password");
+				if (password.Length <= 4)
+					throw new ArgumentException("Password too short, need more than 4 chars.", "password");
+				if (String.IsNullOrEmpty(email))
+					throw new ArgumentException("Value cannot be null or empty.", "email");
+				if (db.Users.Any(u => u.Email == email))
+					throw new ArgumentException("This email is already registred.", "email");
+				if (db.Users.Any(u => u.Nickname == nickname))
+					throw new ArgumentException("This nickname is already registred.", "nickname");
+				try {
+					var u = new EntityLib.User();
+					u.Name = userName;
+					u.Nickname = nickname;
+					u.Password = password;
+					u.Email = email;
+					u.TwitterId = "";
+					u.Token = "";
+					u.Secret = "";
+					db.Users.AddObject(u);
+					db.SaveChanges();
+					return true;
+				} catch (Exception) {
+					return false;
+				}
+			}
+
+			public bool ChangePassword(string nickname, string oldPassword, string newPassword) {
+				if (String.IsNullOrEmpty(nickname)) throw new ArgumentException("Value cannot be null or empty.", "nickname");
+				if (String.IsNullOrEmpty(oldPassword)) throw new ArgumentException("Value cannot be null or empty.", "oldPassword");
+				if (String.IsNullOrEmpty(newPassword)) throw new ArgumentException("Value cannot be null or empty.", "newPassword");
+
+				try {
+					db.Users.Where(u => u.Nickname == nickname).FirstOrDefault().Password = newPassword;
+					db.SaveChanges();
+					return true;
+				} catch (Exception) {
+					return false;
+				}
+			}
+			public void SignIn(string userName, bool createPersistentCookie) {
+				if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+
+				FormsAuthentication.SetAuthCookie(userName, createPersistentCookie);
+
+			}
+
+			public void SignOut() {
+				FormsAuthentication.SignOut();
+			}
+
+			public void UpdateTwitterAccount(OAuthTokenResponse atoken, EntityLib.User usr) {
+				usr.Secret = atoken.TokenSecret;
+				usr.Token = atoken.Token;
+				db.SaveChanges();
+			}
+
+			public EntityLib.User getUser(string s) {
+				return db.Users.First(u => u.Nickname == s);
+			}
+		}
+		public class ReturnUrl {
+			public ReturnUrl(string s) {
+				returnUrl = s;
+			}
+			public string returnUrl { get; set; }
+		}
+		#endregion
+
+}
